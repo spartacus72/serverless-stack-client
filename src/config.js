235bf880b18a08ad47df8f3923cdd49f82405f24
spartacus72 <jeff@cumpsty.co.uk>
2020-10-25 13:58:34 +0000

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: 'us-east-1',
+    BUCKET: 'binary-notes-app-uploads'
+  },
+  apiGateway: {
+    REGION: 'us-east-1',
+    URL: 'https://7tqadzone4.execute-api.us-east-1.amazonaws.com/prod'
+  },
+  cognito: {
+    REGION: 'us-east-1',
+    USER_POOL_ID: 'us-east-1_LP5qdIJE7',
+    APP_CLIENT_ID: 'bihsnk18jc94linao0icp9dbm',
+    IDENTITY_POOL_ID: 'us-east-1:be3cab28-a605-4b3f-bdf2-c0ae4b36be85'
+  }
+}
---
+++ src/containers/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './NotFound.css';
+
+const NotFound = () =>
+  <div className="NotFound">
+    <h3>Sory, page not found!</h3>
+  </div>
+
+export default NotFound;
